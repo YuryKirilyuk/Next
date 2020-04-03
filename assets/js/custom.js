72fb5830446401fb383a4f9d4d067e282a3f1955
Yury Kirilyuk <yury@cyberiansoft.com>
2020-04-04 02:03:57 +0300

    landing + crisis

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -40,4 +40,65 @@ jQuery(function($){
     });
 
 
+    $('#more-seed').on('click', function(){
+        var btn = $(this),
+            sSeed = $('.section-seed-stage').offset();
+
+        $('#seed').slideToggle();
+        btnChange(btn, sSeed);
+        return false;
+    });
+
+    $('#more-board').on('click', function(){
+        var btn = $(this),
+            sBoard = $('.section-board-bundle').offset();
+
+        $('#board').slideToggle();
+        btnChange(btn, sBoard);
+        return false;
+    });
+
+    $('#more-p1').on('click', function(){
+        var btn = $(this),
+            sSeed = $('.section-package-1').offset();
+
+        $('#package-1').slideToggle();
+        btnChange(btn, sSeed);
+        return false;
+    });
+
+    $('#more-p2').on('click', function(){
+        var btn = $(this),
+            sBoard = $('.section-package-2').offset();
+
+        $('#package-2').slideToggle();
+        btnChange(btn, sBoard);
+        return false;
+    });
+
+    $('#more-p3').on('click', function(){
+        var btn = $(this),
+            sSeed = $('.section-package-3').offset();
+
+        $('#package-3').slideToggle();
+        btnChange(btn, sSeed);
+        return false;
+    });
+
+    $('#more-p4').on('click', function(){
+        var btn = $(this),
+            sBoard = $('.section-package-4').offset();
+
+        $('#package-4').slideToggle();
+        btnChange(btn, sBoard);
+        return false;
+    });
+
+
+    function btnChange(btn, offset){
+        btn.toggleClass('visible');
+        btn.hasClass('visible') ? btn.text('Close') : (btn.text('More'), $("html, body").animate({ scrollTop: offset.top - 180 }));
+    }
+
+
 });
